3a2b8f56fc0dfac5eb7b9b2d7370054bf63d6787
BarnabasLim <blim048@e.ntu.edu.sg>
2023-05-21 23:16:08 +0800

    [Feature] [04_Dynamic Style] 4.2 add stwitching functions

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
+//Constants
+import { THEME } from 'src/theme/theme.constant';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,4 +10,29 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'inputVal';
+  constructor(
+    @Inject(DOCUMENT) private document:Document
+  ){}
+
+  public theme_id=0
+  loopTheme(){
+    let list_of_themes=Object.keys(THEME)
+    this.theme_id+=1;
+    if (this.theme_id>=list_of_themes.length){
+      this.theme_id=0
+    }
+    this.switchTheme(THEME[list_of_themes[this.theme_id]])
+    console.log(list_of_themes,THEME[list_of_themes[this.theme_id]])
+  }
+
+  /**
+   * switches href attribute of <link id='app-theme'> in html <head> 
+   * @param theme (string) name for element found in dist
+   */
+  switchTheme(theme:string){
+    let themeLink=this.document.getElementById('app-theme') as HTMLLinkElement;
+    if(themeLink){
+      themeLink.href=theme
+    }
+  }
 }
