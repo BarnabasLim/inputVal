b66fe8ffff6b409dd8a2d8ca7928228ea1f3ae0c
BarnabasLim <blim048@e.ntu.edu.sg>
2023-05-21 23:15:41 +0800

    [Feature] [04_Dynamic Style] 4.1 add theme constants

---
+++ src/theme/theme.constant.ts
@@ -0,0 +1,8 @@
+export const THEME={
+    AD_LIGHT_INDIGO:"ad-light-indigo.css",
+    ARYA_BLUE:"arya-blue.css",
+    BOOTSTRAP4_DARK_PURPLE:"bootstrap4-dark-purple.css",
+    BOOTSTRAP4_LIGHT_PURPLE:"bootstrap4-light-purple.css",
+    SAGA_BLUE:"saga-blue.css",
+    VELA_BLUE:"vela-blue.css"
+}
