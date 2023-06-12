23547ab0c494859721b0e77cb08f0ab97153b6df
BarnabasLim <blim048@e.ntu.edu.sg>
2023-06-13 00:56:39 +0800

    [s04] conditional-validators

--- src/app/components/s00-sandbox/s00-sandbox.component.ts
+++ src/app/components/s00-sandbox/s00-sandbox.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { forbiddenNameValidator } from './validators/forbiddenName.validator';
 import { asyncDuplicateCheck } from './validators/asyncDuplicateCheck.validator';
+import { conditionalValidator } from './validators/conditionalValidator.validator';
 @Component({
   selector: 'app-s00-sandbox',
   templateUrl: './s00-sandbox.component.html',
@@ -10,6 +11,7 @@ import { asyncDuplicateCheck } from './validators/asyncDuplicateCheck.validator'
 export class S00SandboxComponent implements OnInit {
 
   public checked:boolean=false;
+  public name_subscriber:any=null;
 
   public myForm:FormGroup;
   constructor(
@@ -20,14 +22,32 @@ export class S00SandboxComponent implements OnInit {
     this.myForm=this.fb.group({
       name:['',[
         Validators.required, 
-        forbiddenNameValidator(['na','nil','null'])
       ]],
-      alias:['',{
-        validators: [Validators.required],
-        asyncValidators: [asyncDuplicateCheck],
-        updateOn: 'blur'
-      }]
+      alias:['',[
+        conditionalValidator(
+            ()=>{return this.myForm?.get('name')?.value?.toLowerCase()=="secret"}, 
+            Validators.required
+        )
+      ]]
     })
+
+    this.subscriber()
+  }
+
+  ngOnDestroy():void{
+    this.unsubscriber()
+  }
+
+  subscriber(){
+    this.name_subscriber=this.myForm.get('name').valueChanges.subscribe((value)=>{
+      this.myForm.get('alias').updateValueAndValidity();
+    })
+  }
+
+  unsubscriber(){
+    if(!this.name_subscriber){
+      this.name_subscriber.unsubscribe();
+    }
   }
 
   get name(){
---
+++ src/app/components/s00-sandbox/validators/conditionalValidator.validator.ts
@@ -0,0 +1,22 @@
+import { AbstractControl, ValidationErrors, ValidatorFn, Validators } from "@angular/forms";
+
+
+// conditionalValidator(
+//     ()=>{return this.myForm?.get('name')?.value?.toLowerCase()=="secret"}, 
+//     Validators.required
+// )
+export function conditionalValidator(conditionalFunction:()=>boolean, validator:ValidatorFn):ValidatorFn|null{
+
+    return (control:AbstractControl):ValidationErrors|null=>{
+        console.log("conditionalVaidator Function Called")
+        if(conditionalFunction()){//Evaluate to True or False
+            return validator(control);
+            //Evaluate and returns a ValidationErrors 
+            //e.g. Validators.required(control) => {required:true}|null
+            //e.g. Validators.maxLength(control) => {maxlength:{requiredLength:11, actuallength:26}}|null
+        }else{
+            return null
+        }
+    }
+
+}
